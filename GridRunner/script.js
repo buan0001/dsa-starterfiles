da8f9acd485c5bb4d695d8b122335c228e2022e8
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-09-30 15:00:58 +0200

    can move around in snake

--- GridRunner/script.js
+++ GridRunner/script.js
@@ -8,18 +8,68 @@ window.addEventListener("load", start);
 function start() {
   console.log(`Javascript kører`);
 
+  document.addEventListener("keydown", keyPressed);
   // start ticking
   tick();
 }
 
+let direction = "a";
+
 function tick() {
   // setup next tick
-  setTimeout(tick, 500);
+  setTimeout(tick, 200);
 
-  // TODO: Do stuff
+  switch (direction) {
+    case "arrowleft":
+    case "a":
+      moveLeft();
+      break;
+    case "arrowright":
+    case "d":
+      moveRight();
+      break;
+    case "arrowup":
+    case "w":
+      moveUp();
+      break;
+    case "arrowdown":
+    case "s":
+      moveDown();
+      break;
+  }
+
+  writeToCell(player.row, player.col, 1);
 
   // display the model in full
   displayBoard();
+}
+
+function moveUp() {
+  writeToCell(player.row--, player.col, 0);
+  if (player.row < 0) {
+    player.row = 9;
+  }
+}
+
+function moveDown() {
+  writeToCell(player.row++, player.col, 0);
+  if (player.row > 9) {
+    player.row = 0;
+  }
+}
+
+function moveLeft() {
+  writeToCell(player.row, player.col--, 0);
+  if (player.col < 0) {
+    player.col = 9;
+  }
+}
+
+function moveRight() {
+  writeToCell(player.row, player.col++, 0);
+  if (player.col > 9) {
+    player.col = 0;
+  }
 }
 
 // #endregion controller
@@ -38,6 +88,8 @@ const model = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
+
+const player = { row: 5, col: 5 };
 
 function writeToCell(row, col, value) {
   model[row][col] = value;
@@ -64,12 +116,25 @@ function displayBoard() {
           break;
         case 1: // Note: doesn't remove goal if previously set
           cells[index].classList.add("player");
+          cells[index].classList.remove("goal");
           break;
         case 2: // Note: doesn't remove player if previously set
           cells[index].classList.add("goal");
+          cells[index].classList.remove("player");
           break;
       }
     }
+  }
+}
+
+function keyPressed(event) {
+  console.log("key pressed:",event);
+  const key = event.key.toLowerCase();
+  console.log(key);
+  
+  const validInput = ["arrowup", "arrowdown","arrowleft","arrowright", "w", "a","s","d"]
+  if (validInput.some(validKey => validKey == key)) {
+    direction = key;
   }
 }
 
