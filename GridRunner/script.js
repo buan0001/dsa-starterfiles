72be4129f8704507b2a2d2d5495fdd19cfed9977
buan0001 <123996422+buan0001@users.noreply.github.com>
2024-09-30 15:39:46 +0200

    can move with a longer snake

--- GridRunner/script.js
+++ GridRunner/script.js
@@ -14,61 +14,88 @@ function start() {
 }
 
 let direction = "a";
+// const head = { row: 5, col: 5 };
+const queue = [
+  { row: 5, col: 5 },
+  // { row: 5, col: 6 },
+  // { row: 5, col: 7 },
+];
 
 function tick() {
   // setup next tick
-  setTimeout(tick, 200);
+  setTimeout(tick, 600);
+
+  for (const part of queue) {
+    writeToCell(part.row, part.col, 0);
+  }
+
+  const oldHead = {
+    row: queue[queue.length - 1].row,
+    col: queue[queue.length - 1].col,
+  };
 
   switch (direction) {
     case "arrowleft":
     case "a":
-      moveLeft();
+      moveLeft(oldHead);
       break;
     case "arrowright":
     case "d":
-      moveRight();
+      moveRight(oldHead);
       break;
     case "arrowup":
     case "w":
-      moveUp();
+      moveUp(oldHead);
       break;
     case "arrowdown":
     case "s":
-      moveDown();
+      moveDown(oldHead);
       break;
   }
 
-  writeToCell(player.row, player.col, 1);
+  if (queue.length >= 3) {
+    queue.shift();
+    // writeToCell(deleted.row, deleted.col, 0);
+    queue.push(oldHead);
+    // queue.push({ ...oldHead });
+  } else if (queue.length < 3) {
+    queue.push({ ...oldHead });
+  }
+  for (const tile of queue) {
+    writeToCell(tile.row, tile.col, 1);
+  }
+  // console.log("queue:", queue);
+  // console.log("length", queue.length);
 
   // display the model in full
   displayBoard();
 }
 
-function moveUp() {
-  writeToCell(player.row--, player.col, 0);
-  if (player.row < 0) {
-    player.row = 9;
+function moveUp(head) {
+  writeToCell(head.row--, head.col, 0);
+  if (head.row < 0) {
+    head.row = 9;
   }
 }
 
-function moveDown() {
-  writeToCell(player.row++, player.col, 0);
-  if (player.row > 9) {
-    player.row = 0;
+function moveDown(head) {
+  writeToCell(head.row++, head.col, 0);
+  if (head.row > 9) {
+    head.row = 0;
   }
 }
 
-function moveLeft() {
-  writeToCell(player.row, player.col--, 0);
-  if (player.col < 0) {
-    player.col = 9;
+function moveLeft(head) {
+  writeToCell(head.row, head.col--, 0);
+  if (head.col < 0) {
+    head.col = 9;
   }
 }
 
-function moveRight() {
-  writeToCell(player.row, player.col++, 0);
-  if (player.col > 9) {
-    player.col = 0;
+function moveRight(head) {
+  writeToCell(head.row, head.col++, 0);
+  if (head.col > 9) {
+    head.col = 0;
   }
 }
 
@@ -88,8 +115,6 @@ const model = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
-
-const player = { row: 5, col: 5 };
 
 function writeToCell(row, col, value) {
   model[row][col] = value;
@@ -128,11 +153,11 @@ function displayBoard() {
 }
 
 function keyPressed(event) {
-  console.log("key pressed:",event);
+  // console.log("key pressed:",event);
   const key = event.key.toLowerCase();
-  console.log(key);
-  
-  const validInput = ["arrowup", "arrowdown","arrowleft","arrowright", "w", "a","s","d"]
+  // console.log(key);
+
+  const validInput = ["arrowup", "arrowdown", "arrowleft", "arrowright", "w", "a", "s", "d"];
   if (validInput.some(validKey => validKey == key)) {
     direction = key;
   }
